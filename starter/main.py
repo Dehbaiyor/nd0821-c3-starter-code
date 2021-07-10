f273ae1ba6445f67680ebef38023eb0d479720e2
Dehbaiyor <dehbaiyor@gmail.com>
2021-07-10 17:46:10 +0100

    Added Heroku config

--- starter/main.py
+++ starter/main.py
@@ -8,6 +8,14 @@ import joblib
 from starter.ml.model import *
 from starter.ml.data import *
 
+import os
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+    
 class Input(BaseModel):
     age : int
     workclass : str
